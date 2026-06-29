43acc8e6be3855fda96c9b46464103ff1e7899b8
Anusha09242 <anushapothu302@gmail.com>
2026-06-29 19:24:36 +0530

    roboshop-rabbitmq

--- 04.rabbitmq.sh
+++ 04.rabbitmq.sh
@@ -29,13 +29,13 @@ VALIDATE(){
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding Rabbitmq Repo"
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>> $LOGS_FILE
 VALIDATE $? "Installing Rabbitmq Server"
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>> $LOGS_FILE
+systemctl start rabbitmq-server &>> $LOGS_FILE
 VALIDATE $? "Enable and Start Rabbitmq Server"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
 VALIDATE $? "Setting up the username and password"
