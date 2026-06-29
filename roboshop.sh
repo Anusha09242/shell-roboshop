238ffd2211c54d56e93f3a40933bd22cce2d50e9
Anusha09242 <anushapothu302@gmail.com>
2026-06-29 16:53:29 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -32,26 +32,26 @@ do
 
     #### Updating R53 Record ####
     aws route53 change-resource-record-sets \
-        --hosted-zone-id $ZONE_ID \
-        --change-batch '
-    {
-        "Comment": "Update A record to add new IP",
-        "Changes": [
-            {
-                "Action": "UPSERT",
-                "ResourceRecordSet": {
-                    "Name": " '$R53_RECORD' ",
-                    "Type": "A",
-                    "TTL": 1,
-                    "ResourceRecords": [
-                        {
-                            "Value": "$IP"
-                        }
-                    ]
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+        {
+            "Comment": "Update A record to add new IP",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": " '$R53_RECORD' ",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "$IP"
+                            }
+                        ]
+                    }
                 }
-            }
-        ]
-    }
+            ]
+        }
 
-'
+    '
 done
