cfa904e8b1d8a07863217945aa156c8c27f8ca2a
Anusha09242 <anushapothu302@gmail.com>
2026-07-05 16:42:19 +0530

    roboshop-frontend

--- 10.frontend.sh
+++ 10.frontend.sh
@@ -28,25 +28,25 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+dnf module disable nginx -y &>> $LOGS_FILE
+dnf module enable nginx:1.24 -y &>> $LOGS_FILE
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "Installing Nginx"
 
 rm -rf /usr/share/nginx/html/*
 VALIDATE $? "Removed Default code"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip  &>> $LOGS_FILE
+cd /usr/share/nginx/html  &>> $LOGS_FILE
+unzip /tmp/frontend.zip &>> $LOGS_FILE
 VALIDATE $? "Downloaded and Extracted frontend code"
 
 rm -f /etc/nginx/nginx.conf
 VALIDATE $? "Removed Default conf"
 
-cp nginx.conf /etc/nginx/nginx.conf
+cp $SCRPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copied roboshop nginx conf"
 
 systemctl restart nginx 
-systemctl enable nginx
+systemctl enable nginx &>> $LOGS_FILE
 VALIDATE $? "Enabled and Restarted nginx"
